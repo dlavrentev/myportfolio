db42e89020ba10bdc3a425ff5c82e59de590fa77
dlavrentev <dimitrilavrentev96@gmail.com>
2022-07-02 04:57:42 +0200

    added cta component, updated header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA';
+imoo
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Dimitri</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+        <div className="me">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </header>
   )
+  
 }
 
 export default Header
