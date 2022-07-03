5d03daf98cd33347e3110f9dd4f64bf5764f218e
dlavrentev <dimitrilavrentev96@gmail.com>
2022-07-03 13:51:54 +0200

    Update Portfolio.jsx

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import './Portfolio.css'
+import React from "react";
+import "./Portfolio.css";
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image"></div>
+            <h3>This is a portfolio item title</h3>
+            <a href="https://github.com" className="btn" target='_blank'>
+              Github
+            </a>
+            <a href="https://github.com" className="btn btn-primary" target='_blank'>
+              Live Demo
+            </a>
+          </article>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
